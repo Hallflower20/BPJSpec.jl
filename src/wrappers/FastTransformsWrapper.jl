99eef4b711044cb5772f6bb3ffacb47399b306c1
Hallflower20 <xhall@caltech.edu>
2024-02-09 05:15:33 +0000

    undo changes

--- src/wrappers/FastTransformsWrapper.jl
+++ src/wrappers/FastTransformsWrapper.jl
@@ -101,15 +101,26 @@ end
 
 function Base.getindex(alm::Alm_Complex, l, m)
     idx = l - m + 1
-    jdx = m + 1
-    return alm.matrix[idx, jdx]
+    jdx = 2m + 1
+    if m == 0
+        return alm.matrix[idx, jdx]
+    else
+        return (alm.matrix[idx, jdx] + alm.matrix[idx, jdx-1]) / √2
+    end
 end
 
 function Base.setindex!(alm::Alm_Complex, value, l, m)
     idx = l - m + 1
-    jdx = m + 1
-    alm.matrix[idx, jdx] = value
-    return value
+    jdx = 2m + 1
+    if m == 0
+        alm.matrix[idx, jdx] = real(value)
+        return value
+    else
+        sqrt2 = √2
+        alm.matrix[idx, jdx]   = real(value) * sqrt2
+        alm.matrix[idx, jdx-1] = imag(value) * sqrt2 * 1im
+        return value
+    end
 end
 
 struct Map <: AbstractMatrix{Float64}
