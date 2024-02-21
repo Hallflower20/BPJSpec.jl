476e5a4f709cd500fc57dcdc29caf71e4fbb07ea
Hallflower20 <xhall@caltech.edu>
2024-02-21 21:39:48 +0000

    m == 0 returns real after setting.

--- src/wrappers/FastTransformsWrapper.jl
+++ src/wrappers/FastTransformsWrapper.jl
@@ -114,7 +114,7 @@ function Base.setindex!(alm::Alm_Complex, value, l, m)
     jdx = m + 1
     if m == 0
         alm.matrix[idx, jdx] = real(value)
-        return value
+        return complex(real(value))
     else
         alm.matrix[idx, jdx] = value * √2
         return value
